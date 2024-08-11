8769b85b0c655e6d2f2ca7d227924c4bc65ce780
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-08-11 15:52:58 +0200

    [1.2.1] - 2024-08-11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.2.1] - 2024-08-11
+
+### Fixed
+
+- gui version
+
 ## [1.2.0] - 2024-08-11
 
 ### Added
--- src/main/java/org/fugerit/java/client/redis/gui/ClientRedisGUI.java
+++ src/main/java/org/fugerit/java/client/redis/gui/ClientRedisGUI.java
@@ -26,7 +26,7 @@ import org.slf4j.LoggerFactory;
 
 public class ClientRedisGUI extends JFrame implements WindowListener, ActionListener {
 
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.2.1";
 	
 	private static final Logger logger = LoggerFactory.getLogger(ClientRedisGUI.class);
 
